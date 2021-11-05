b7c3c9fa2777c25d8555eb72e9d1e1513206b678
RacmanT <tibor.racman@gmail.com>
2021-11-05 17:51:34 +0100

    Contacts list updated

--- app/src/main/java/it/units/businesscardwallet/ContactList.java
+++ app/src/main/java/it/units/businesscardwallet/ContactList.java
@@ -1,5 +1,6 @@
 package it.units.businesscardwallet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,25 +8,23 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
 
-public class ContactList extends Fragment implements AdapterView.OnItemClickListener {
+
+public class ContactList extends Fragment  {
 
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private ListView contactList;
-    private Contact[] contacts = {new Contact("Eren", "Jaeger", "Attack Titan", "singekyno@kyogin.su", 212212, "Paradise Island")};
+    private ArrayList<Contact> contacts = new ArrayList<>();
 
-
-    private String mParam1;
-    private String mParam2;
 
     public ContactList() {
         // Required empty public constructor
@@ -35,8 +34,6 @@ public class ContactList extends Fragment implements AdapterView.OnItemClickList
     public static ContactList newInstance(String param1, String param2) {
         ContactList fragment = new ContactList();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -44,10 +41,6 @@ public class ContactList extends Fragment implements AdapterView.OnItemClickList
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,17 +53,42 @@ public class ContactList extends Fragment implements AdapterView.OnItemClickList
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        contacts.add(new Contact("Eren", "Jaeger", "Attack Titan", "singekyno@kyogin.su", 212212, "Paradise Island"));
+        contacts.add(new Contact("Mikasa", "Ackerman", "EREH support", "mikasa@kyogin.su", 212212, "Paradise Island"));
+
+
         ListView listView = view.findViewById(R.id.contact_list);
-        ArrayAdapter<Contact> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,contacts);
+        ContactAdapter adapter = new ContactAdapter(getActivity(), contacts);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
     }
 
 
 
+
+
+}
+
+class ContactAdapter extends ArrayAdapter<Contact> {
+
+    public ContactAdapter(Context context, ArrayList<Contact> contact) {
+        super(context, 0, contact);
+    }
+
+
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(),contacts[position].getName(),Toast.LENGTH_SHORT).show();
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Contact contact = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_contact_row, parent, false);
+        }
+        TextView viewName =  convertView.findViewById(R.id.row_name);
+        TextView viewLastName =  convertView.findViewById(R.id.row_last_name);
+        viewName.setText(contact.getName());
+        viewLastName.setText(contact.getLastName());
+
+        convertView.setOnClickListener(v -> Toast.makeText(v.getContext(), viewName.getText().toString(), Toast.LENGTH_SHORT).show());
+        return convertView;
+
     }
 
 }
